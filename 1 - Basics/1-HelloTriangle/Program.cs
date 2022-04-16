39cf4d8a9a72d81ba81d844e3b6c02727c5add58
Aleksandr Brown <brown-aleks@mail.ru>
2022-04-16 22:08:50 +0400

    HelloTriangl

---
+++ 1 - Basics/1-HelloTriangle/Program.cs
@@ -0,0 +1,28 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using System;
+
+namespace _1_HelloTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NativeWindowSettings nativeWindowSettings = new NativeWindowSettings()
+            {
+                Size = new Vector2i(800, 600),
+                Location = new Vector2i(100, 100),
+                Title = "CreatingWindow",
+                APIVersion = new Version(4, 6),
+                Flags = ContextFlags.ForwardCompatible,
+                Profile = ContextProfile.Core
+            };
+
+            using (Window game = new Window(nativeWindowSettings))
+            {
+                game.Run();
+            }
+        }
+    }
+}
---
+++ 1 - Basics/1-HelloTriangle/Shaders/shader.frag
@@ -0,0 +1,8 @@
+#version 330
+
+out vec4 outputColor;
+
+void main()
+{
+    outputColor = vec4(0.8, 0.4, 0.0, 1.0);
+}
