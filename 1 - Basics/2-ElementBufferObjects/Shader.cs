2220ef23651b6102d26383558e54d70f9cd0fe01
Aleksandr Brown <brown-aleks@mail.ru>
2022-04-17 23:39:49 +0400

    2-ElementBufferObjects

---
+++ 1 - Basics/2-ElementBufferObjects/Shader.cs
@@ -0,0 +1,197 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _2_ElementBufferObjects
+{
+    // Простой класс, предназначенный для помощи в создании шейдеров. https://github.com/opentk/LearnOpenTK/blob/master/Common/Shader.cs
+    public class Shader
+    {
+        public readonly int Handle;
+        private readonly Dictionary<string, int> _uniformLocations;
+
+        // Вот как вы создаете простой шейдер.
+        // Шейдеры написаны на GLSL, языке, очень похожем на C по своей семантике.
+        // Исходный код GLSL компилируется *во время выполнения*, поэтому он может оптимизировать себя для видеокарты,
+        // на которой он в данный момент используется.
+        // Пример GLSL с комментариями можно найти в shaper.vert.
+        public Shader(string vertPath, string fragPath)
+        {
+            // Существует несколько различных типов шейдеров, но для базового рендеринга вам нужны только два — вершинный и фрагментный шейдеры.
+            // Вершинный шейдер отвечает за перемещение по вершинам и загрузку этих данных во фрагментный шейдер.
+            // Здесь вершинный шейдер не будет слишком важен, но позже он станет более важным.
+            // Фрагментный шейдер отвечает за последующее преобразование вершин во «фрагменты», которые представляют все данные,
+            // необходимые OpenGL для отрисовки пикселя.
+            // Фрагментный шейдер — это то, что мы будем здесь использовать больше всего.
+
+            // Загружаем вершинный шейдер и компилируем
+            var vertShaderSource = File.ReadAllText(vertPath);
+            // Создаём пустой шейдер (очевидно). Перечисление ShaderType указывает, какой тип шейдера будет создан.
+            var vertexShader = GL.CreateShader(ShaderType.VertexShader);
+            // Теперь привяжем исходный код GLSL
+            GL.ShaderSource(vertexShader, vertShaderSource);
+            // А затем скомпилировать
+            CompileShader(vertexShader);
+
+            // Загружаем фрагментный шейдер и компилируем
+            var fragShaderSource = File.ReadAllText(fragPath);
+            var fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fragmentShader, fragShaderSource);
+            CompileShader(fragmentShader);
+
+            // Затем эти два шейдера должны быть объединены в шейдерную программу, которую затем может использовать OpenGL.
+            // Для этого создаём программу...
+            Handle = GL.CreateProgram();
+
+            // Прикрепляем оба шейдера...
+            GL.AttachShader(Handle, vertexShader);
+            GL.AttachShader(Handle, fragmentShader);
+
+            // А затем связываем их вместе.
+            LinkProgram(Handle);
+
+            // Когда шейдерная программа связана, ей больше не нужны прикрепленные к ней отдельные шейдеры;
+            // скомпилированный код копируется в шейдерную программу.
+            // Отсоединяем их, а затем удаляем.
+            GL.DetachShader(Handle, vertexShader);
+            GL.DetachShader(Handle, fragmentShader);
+            GL.DeleteShader(fragmentShader);
+            GL.DeleteShader(vertexShader);
+
+            // Теперь шейдер готов к работе, но сначала мы собираемся кэшировать все местоположения юниформ шейдера.
+            // Запрос этого из шейдера очень медленный, поэтому мы делаем это один раз при инициализации и повторно используем эти значения
+            // потом.
+
+            // Во-первых, нам нужно получить количество активных юниформ в шейдере.
+            GL.GetProgram(Handle, GetProgramParameterName.ActiveUniforms, out var numberOfUniforms);
+
+            // Затем выделяем словарь для хранения местоположений.
+            _uniformLocations = new Dictionary<string, int>();
+
+            // Перебираем все униформы,
+            for (var i = 0; i < numberOfUniforms; i++)
+            {
+                // получаем имя этой униформы,
+                var key = GL.GetActiveUniform(Handle, i, out _, out _);
+                // получить местоположение,
+                var location = GL.GetUniformLocation(Handle, key);
+                // и затем добавляем его в словарь.
+                _uniformLocations.Add(key, location);
+            }
+        }
+
+        private static void CompileShader(int shader)
+        {
+            // Пытаемся скомпилировать шейдер
+            GL.CompileShader(shader);
+
+            // Проверяем ошибки компиляции
+            GL.GetShader(shader, ShaderParameter.CompileStatus, out var code);
+            if (code != (int)All.True)
+            {
+                // Мы можем использовать GL.GetShaderInfoLog(shader), чтобы получить информацию об ошибке.
+                var infoLog = GL.GetShaderInfoLog(shader);
+                throw new Exception($"Error occurred whilst compiling Shader({shader}).\n\n{infoLog}");
+            }
+        }
+
+        private static void LinkProgram(int program)
+        {
+            // Связываем программу
+            GL.LinkProgram(program);
+
+            // Проверяем ошибки линковки
+            GL.GetProgram(program, GetProgramParameterName.LinkStatus, out var code);
+            if (code != (int)All.True)
+            {
+                // Мы можем использовать GL.GetProgramInfoLog (программа), чтобы получить информацию об ошибке.
+                var infoLog = GL.GetProgramInfoLog(program);
+                throw new Exception($"Error occurred whilst linking Program({program}).\n\n{infoLog}");
+            }
+        }
+
+        // Функция-оболочка, которая включает программу шейдера.
+        public void Use()
+        {
+            GL.UseProgram(Handle);
+        }
+
+        // Источники шейдеров, предоставленные с этим проектом, используют жестко запрограммированный layout (location) -s. Если вы хотите сделать это динамически,
+        // вы можете опустить строки макета (location = X) в вершинном шейдере и использовать их в VertexAttribPointer вместо жестко заданных значений.
+        public int GetAttribLocation(string attribName)
+        {
+            return GL.GetAttribLocation(Handle, attribName);
+        }
+
+        // Установщики униформы
+        // Униформы - это переменные, которые могут быть установлены пользовательским кодом вместо чтения их из VBO.
+        // Вы используете VBO для данных, связанных с вершинами, и униформы почти для всего остального.
+
+        // Установка униформы почти всегда одна и та же, поэтому я объясню это здесь один раз, а не в каждом методе:
+        // 1. Свяжите программу, на которую хотите установить униформу.
+        // 2. Получить дескриптор местоположения униформы с помощью GL.GetUniformLocation.
+        // 3. Используйте соответствующую функцию GL.Uniform *, чтобы установить униформу.
+
+        /// <summary>
+        /// Устанавливает униформу int на этом шейдере.
+        /// </summary>
+        /// <param name="name">Название униформы</param>
+        /// <param name="data">Данные для установки</param>
+        public void SetInt(string name, int data)
+        {
+            GL.UseProgram(Handle);
+            GL.Uniform1(_uniformLocations[name], data);
+        }
+
+        /// <summary>
+        /// Устанавливает униформу float на этом шейдере.
+        /// </summary>
+        /// <param name="name">Название униформы</param>
+        /// <param name="data">Данные для установки</param>
+        public void SetFloat(string name, float data)
+        {
+            GL.UseProgram(Handle);
+            GL.Uniform1(_uniformLocations[name], data);
+        }
+
+        /// <summary>
+        /// Устанавливает униформу Matrix4 на этом шейдере.
+        /// </summary>
+        /// <param name="name">Название униформы</param>
+        /// <param name="data">Данные для установки</param>
+        /// <remarks>
+        ///   <para>
+        ///   Матрица транспонируется перед отправкой в шейдер.
+        ///   </para>
+        /// </remarks>
+        public void SetMatrix4(string name, Matrix4 data)
+        {
+            GL.UseProgram(Handle);
+            GL.UniformMatrix4(_uniformLocations[name], true, ref data);
+        }
+
+        /// <summary>
+        /// Устанавливает униформу Vector3 на этом шейдере.
+        /// </summary>
+        /// <param name="name">Название униформы</param>
+        /// <param name="data">Данные для установки</param>
+        public void SetVector3(string name, Vector3 data)
+        {
+            GL.UseProgram(Handle);
+            GL.Uniform3(_uniformLocations[name], data);
+        }
+
+        /// <summary>
+        /// Устанавливает униформу Vector4 на этом шейдере.
+        /// </summary>
+        /// <param name="name">Название униформы</param>
+        /// <param name="data">Данные для установки</param>
+        public void SetVector4(string name, Vector4 data)
+        {
+            GL.UseProgram(Handle);
+            GL.Uniform4(_uniformLocations[name], data);
+        }
+    }
+}
---
+++ 1 - Basics/2-ElementBufferObjects/Window.cs
@@ -0,0 +1,144 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using OpenTK.Windowing.Desktop;
+
+namespace _2_ElementBufferObjects
+{
+    // Итак, вы нарисовали первый треугольник. Но как насчет рисования нескольких?
+    // Вы можете просто добавить больше вершин в массив, и это технически сработает, но предположим, что вы рисуете прямоугольник.
+    // Для этого нужны только четыре вершины, но поскольку OpenGL работает с треугольниками, вам нужно определить 6.
+    // Ничего особенного, но быстро складывается, когда вы переходите к более сложным моделям. Например, кубу нужно всего 8 вершин, но
+    // для этого потребуется 36 вершин!
+
+    // OpenGL предоставляет способ повторного использования вершин, что может значительно снизить использование памяти сложными объектами.
+    // Это называется объектом-буфером элемента. Это руководство будет посвящено тому, как его настроить.
+    public class Window : GameWindow
+    {
+        // Мы модифицируем массив вершин, чтобы включить четыре вершины для нашего прямоугольника.
+        private readonly float[] _vertices =
+        {
+             0.5f,  0.5f, 0.0f, // в правом верхнем углу
+             0.5f, -0.5f, 0.0f, // внизу справа
+            -0.5f, -0.5f, 0.0f, // Нижняя левая
+            -0.5f,  0.5f, 0.0f, // верхний левый
+        };
+
+        // Затем мы создаем новый массив: index.
+        // Этот массив контролирует, как EBO будет использовать эти вершины для создания треугольников
+        private readonly uint[] _indices =
+        {
+            // Обратите внимание, что индексы начинаются с 0!
+            0, 1, 3, // Первый треугольник будет правой нижней половиной треугольника
+            1, 2, 3  // Тогда вторая будет правая верхняя половина треугольника
+        };
+
+        private int _fps;
+        private int _vertexBufferObject;
+
+        private int _vertexArrayObject;
+
+        private Shader _shader;
+
+        // Добавляем дескриптор EBO
+        private int _elementBufferObject;
+
+        public Window(NativeWindowSettings nativeWindowSettings): base(GameWindowSettings.Default, nativeWindowSettings)
+        {
+            VSync = VSyncMode.On;
+            CursorVisible = true;
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+            _vertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+
+            _vertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(_vertexArrayObject);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            // Мы создаем / связываем EBO объекта буфера элемента так же, как и VBO, за исключением того, что здесь есть существенная разница, которая может ДЕЙСТВИТЕЛЬНО сбить с толку.
+            // Точка привязки для ElementArrayBuffer на самом деле не является глобальной точкой привязки, как ArrayBuffer.
+            // Вместо этого это фактически свойство связанного в данный момент VertexArrayObject, и привязка EBO без VAO является неопределенным поведением.
+            // Это также означает, что если вы привяжете другой VAO, текущий ElementArrayBuffer изменится вместе с ним.
+            // Еще одна хитрость в том, что вам не нужно отвязать буфер в ElementArrayBuffer, так как это сделает отсоединение VAO,
+            // и отмена привязки EBO удалит его из VAO вместо того, чтобы отвязать его, как вы это делали бы для VBO или VAO.
+            _elementBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
+            // Мы также загружаем данные в EBO так же, как и с VBO.
+            GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices, BufferUsageHint.StaticDraw);
+            // EBO теперь правильно настроен. Перейдите к функции Render, чтобы увидеть, как мы теперь рисуем наш прямоугольник!
+
+            _shader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
+            _shader.Use();
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            _shader.Use();
+
+            // Поскольку ElementArrayObject является свойством привязанного в данный момент VAO,
+            // буфер, который вы найдете в ElementArrayBuffer, изменится в соответствии с текущим привязанным VAO.
+            GL.BindVertexArray(_vertexArrayObject);
+
+            // Затем замените вызов DrawTriangles на вызов DrawElements
+            // Аргументы:
+            // Примитивный тип для рисования. Треугольники в данном случае.
+            // Сколько индексов нужно нарисовать. Шесть в этом случае.
+            // Тип данных индексов. Индексы представляют собой беззнаковое целое число, поэтому мы тоже хотим этого здесь.
+            // Смещение в EBO. Установите значение 0, потому что мы хотим нарисовать все целиком.
+
+            GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Line); //Способ отображения    //  PolygonMode.Fill - Отрисовывает примитив заполненным.
+            GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            SwapBuffers();
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            var input = KeyboardState;
+
+            if (input.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+
+            Title = $"Element Buffer Objects FPS - {_fps++}";
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+            GL.Viewport(0, 0, Size.X, Size.Y);
+        }
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+
+            // Отключаем все ресурсы, привязывая цели к 0 / null.
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
+            GL.UseProgram(0);
+
+            // Удаляем все ресурсы.
+            GL.DeleteBuffer(_vertexBufferObject);
+            GL.DeleteVertexArray(_vertexArrayObject);
+
+            GL.DeleteProgram(_shader.Handle);
+        }
+
+    }
+}
