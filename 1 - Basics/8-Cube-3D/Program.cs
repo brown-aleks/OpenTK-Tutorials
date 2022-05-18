600ac6c005da85bb55fdad5fb9001c59a61a360e
Aleksandr Brown <brown-aleks@mail.ru>
2022-05-18 23:20:55 +0400

    _8_Cube_3D

---
+++ 1 - Basics/8-Cube-3D/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace _8_Cube_3D
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            NativeWindowSettings nativeWindowSettings = new NativeWindowSettings()
+            {
+                Size = new Vector2i(800, 600),
+                Location = new Vector2i(100, 100),
+                Title = "Coordinates Systems",
+                APIVersion = new Version(4, 6),
+                Flags = ContextFlags.ForwardCompatible,
+                Profile = ContextProfile.Core
+            };
+
+            using Window game = new(nativeWindowSettings);
+            game.Run();
+        }
+    }
+}
